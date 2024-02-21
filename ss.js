91e8b94d0783b1e637c2b474a44d897a59e72334
VictoriaDorado <127496937+VictoriaDorado@users.noreply.github.com>
2024-02-21 10:56:18 +0100

    Add files via upload

--- ss.js
+++ ss.js
@@ -1,39 +1,52 @@
 const contenedor = document.getElementById('contenedor');
 const mensaje = document.getElementById('mensaje');
-const numeros = ['1', '2', '3', '4', '5', '6', '7', '8'];
-// const numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16'];
+
 let cartas = [];
 let cartasgiradas = [];
 let puedegirar = true;
 
+let numeros;
 
-function establecerDificultad() {
+function establecerDificultad(difficulty) {
+  let cartasporfila, cartasporcolumna;
 
- 
+  if (difficulty === 'bajo') {
+    numeros = ['1', '2', '3', '4', '5', '6', '7', '8'];
+    cartasporfila = 4;
+    cartasporcolumna = 4;
+  } else if (difficulty === 'medio') {
+    numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'];
+    cartasporfila = 6;
+    cartasporcolumna = 6;
+  } else if (difficulty === 'alto') {
+    numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32'];
+    cartasporfila = 8;
+    cartasporcolumna = 8;
+  }
 
-  inicializar();
+  inicializar(cartasporfila, cartasporcolumna);
 }
 
-function inicializar() {
+function inicializar(cartasporfila, cartasporcolumna) {
   var elementoFrases = document.getElementsByClassName('frases')[0];
 
   if (elementoFrases) {
     elementoFrases.style.display = 'none';
-  }    
-  
-    document.getElementById('mensaje').style.display = 'none';
-    document.getElementById('seleccionarDificultad').style.display = 'none';
-    document.getElementById('botontexto').style.display = 'none';
-    mostrarCuentaRegresiva().then(() => {
-    contenedor.style.display = 'flex';
+  }
+
+  document.getElementById('mensaje').style.display = 'none';
+  document.getElementById('seleccionarDificultad').style.display = 'none';
+  document.getElementById('botontexto').style.display = 'none';
+  mostrarCuentaRegresiva().then(() => {
+    contenedor.style.display = 'grid';
+    contenedor.style.gridTemplateColumns = `repeat(${cartasporfila}, 1fr)`;
+    contenedor.style.gridTemplateRows = `repeat(${cartasporcolumna}, 1fr)`;
+    contenedor.style.gridGap = '10px'; // Ajusta el espacio entre las celdas del grid
+
     barajarcartas();
     mostrarmensaje('Encuentra todos los pares de tarjetas.');
   });
 }
-
-
-
-
 
 function mostrarCuentaRegresiva() {
   return new Promise((resolve) => {
@@ -58,55 +71,61 @@ function mostrarCuentaRegresiva() {
   });
 }
 
-
-
-
-
-function barajarcartas() 
-{
+function barajarcartas() {
   document.getElementById('mensaje').style.display = 'block';
-    // Crear una copia de las cartas duplicadas. Tenemos un array con los simbolos duplicados
-    const todascartas = numeros.concat(numeros); //numeros.concat(numeros.slice())
+  // Crear una copia de las cartas duplicadas. Tenemos un array con los simbolos duplicados
+  const todascartas = numeros.concat(numeros); //numeros.concat(numeros.slice())
 
-    // Barajar las cartas
-    for (let i = 0; i < todascartas.length; i++) 
-    {
-        const randomIndex = Math.floor(Math.random() * todascartas.length); // esta línea de código genera un índice aleatorio dentro del rango de los elementos no procesados en el array allCards.
-        // Intercambiar elementos usando una variable temporal
-        const aux = todascartas[i];
-        todascartas[i] = todascartas[randomIndex];
-        todascartas[randomIndex] = aux;
-    }
+  // Barajar las cartas
+  for (let i = 0; i < todascartas.length; i++) {
+    const randomIndex = Math.floor(Math.random() * todascartas.length);
+    // Intercambiar elementos usando una variable temporal
+    const aux = todascartas[i];
+    todascartas[i] = todascartas[randomIndex];
+    todascartas[randomIndex] = aux;
+  }
 
-    // Asignar el array barajado a la variable cartas
-    cartas = todascartas;
+  // Asignar el array barajado a la variable cartas
+  cartas = todascartas;
 
-    // Renderizar las cartas
-    devolvercartas();
+  // Renderizar las cartas
+  devolvercartas();
 }
+
 
 function devolvercartas() {
   contenedor.innerHTML = '';
 
   for (let x = 0; x < cartas.length; x++) {
-  const num = cartas[x];
+    const num = cartas[x];
 
-  const card = document.createElement('div');
-  card.classList.add('card'); // le añadimos una clase
-  card.textContent = num;
-  card.setAttribute('data-index', x);
-  card.addEventListener('click', girarcarta);
+    const card = document.createElement('div');
+    card.classList.add('card'); // le añadimos una clase
+    card.textContent = num;
+    card.setAttribute('data-index', x);
+    card.addEventListener('click', girarcarta);
 
     contenedor.appendChild(card);
   }
+
+  ajustarMargenes();
 }
 
+// Agrega esta función al final del código
+function ajustarMargenes() {
+  const tarjetas = document.querySelectorAll('.card');
+  tarjetas.forEach((tarjeta, index) => {
+    if (cartasgiradas.includes(index)) {
+      tarjeta.classList.add('hidden');
+    }
+  });
+}
 
 // Función para girar una tarjeta
 function girarcarta(event) {
   // Verificar si se puede girar una tarjeta
   if (!puedegirar) {
-      return; // Si no se puede, salir de la función
+    return; // Si no se puede, salir de la función
   }
 
   // Obtener la tarjeta clicada
@@ -117,7 +136,7 @@ function girarcarta(event) {
 
   // Evitar hacer clic en tarjetas ya giradas
   if (cartasgiradas.includes(index)) {
-      return; // Si la tarjeta ya está volteada, salir de la función
+    return; // Si la tarjeta ya está volteada, salir de la función
   }
 
   // Ocultar visualmente la tarjeta clicada
@@ -129,34 +148,32 @@ function girarcarta(event) {
   // Verificar si se han volteado dos tarjetas
   if (cartasgiradas.length === 2) {
     puedegirar = false; // Desactivar la capacidad de voltear temporalmente
-      comprobariguales();
+    comprobariguales();
   }
+}
+
+function comprobariguales() {
+  const indice1 = cartasgiradas[0];
+  const indice2 = cartasgiradas[1];
+  const carta1 = document.querySelector('.card[data-index="' + indice1 + '"]');
+  const carta2 = document.querySelector('.card[data-index="' + indice2 + '"]');
+
+  if (cartas[indice1] === cartas[indice2]) {
+    // si las cartas coinciden vaciamos el array.
+    cartasgiradas = [];
+  } else {
+    // si no coinciden
+    carta1.classList.remove('hidden');
+    carta2.classList.remove('hidden');
+    cartasgiradas = [];
   }
 
+  puedegirar = true;
 
-  function comprobariguales() {
-    const indice1 = cartasgiradas[0];
-    const indice2 = cartasgiradas[1];
-    const carta1 = document.querySelector('.card[data-index="' + indice1 + '"]');
-    const carta2 = document.querySelector('.card[data-index="' + indice2 + '"]');
-
-    if (cartas[indice1] === cartas[indice2]) // si las cartas coinciden vaciamos el array.
-    {
-      cartasgiradas = [];
-    }
-    else // si no coinciden
-    {
-      carta1.classList.remove('hidden');
-      carta2.classList.remove('hidden');
-      cartasgiradas = [];
-    }
-
-    puedegirar = true;
-
-    if (cartasgiradas.length === cartas.length) {
-      contenedor.style.display = 'none';
-      mostrarmensaje("¡Felicidades! Has encontrado todos los pares."); // por alguna razón no funciona
-    }
+  if (cartasgiradas.length === cartas.length) {
+    contenedor.style.display = 'none';
+    mostrarmensaje("¡Felicidades! Has encontrado todos los pares."); 
+  }
 }
 
 function mostrarmensaje(text) {
@@ -164,6 +181,6 @@ function mostrarmensaje(text) {
 }
 
 function mostrarOcultarTexto() {
-    var textoElemento = document.getElementById('texto');
-    textoElemento.style.display = (textoElemento.style.display === 'none') ? 'block' : 'none';
-  }
+  var textoElemento = document.getElementById('texto');
+  textoElemento.style.display = (textoElemento.style.display === 'none') ? 'block' : 'none';
+}
